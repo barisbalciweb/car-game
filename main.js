41829af396de435e937e7459e4cb18f5f4b01021
Baris Balci <barisbalcimusic@gmail.com>
2024-01-22 22:51:05 +0100

    neuigkeiten

--- main.js
+++ main.js
@@ -1,78 +1,84 @@
 //AUTO
 let auto = document.getElementById("auto");
 let autoImg = document.getElementById("auto-img");
+
 let autoPosition = auto.getBoundingClientRect();
 let autoLeft = autoPosition.left;
 let autoTop = autoPosition.top;
 let autoTotalX = autoLeft + autoPosition.width;
 let autoTotalY = autoTop + autoPosition.height;
+
+let isKeyFree = true;
+
 let autoMovInt;
 let speed = 30;
 let distance = 5;
 
 //KEY-DIRECTION-MOVEMENT
 document.addEventListener("keydown", (event) => {
-  clearInterval(autoMovInt);
-  let keyPressed = event.key;
-  switch (keyPressed) {
-    case "ArrowRight":
-      auto.style.transform = "rotate(0)";
-      autoMovInt = setInterval(() => {
-        auto.style.left = autoLeft + distance + "px";
-        autoLeft += distance;
-      }, speed);
-      break;
-    case "ArrowLeft":
-      auto.style.transform = "rotate(0)";
-      autoMovInt = setInterval(() => {
-        auto.style.left = autoLeft - distance + "px";
-        autoLeft -= distance;
-      }, speed);
-      break;
-    case "ArrowDown":
-      auto.style.transform = "rotate(90deg)";
-      autoMovInt = setInterval(() => {
-        auto.style.top = autoTop + distance + "px";
-        autoTop += distance;
-      }, speed);
-      break;
-    case "ArrowUp":
-      auto.style.transform = "rotate(-90deg)";
-      autoMovInt = setInterval(() => {
-        auto.style.top = autoTop - distance + "px";
-        autoTop -= distance;
-      }, speed);
-      break;
+  if (isKeyFree === true) {
+    clearInterval(autoMovInt);
+    let keyPressed = event.key;
+    switch (keyPressed) {
+      case "ArrowRight":
+        auto.style.transform = "rotate(0)";
+        autoMovInt = setInterval(() => {
+          auto.style.left = autoLeft + distance + "px";
+          autoLeft += distance;
+        }, speed);
+        break;
+      case "ArrowLeft":
+        auto.style.transform = "rotate(0)";
+        autoMovInt = setInterval(() => {
+          auto.style.left = autoLeft - distance + "px";
+          autoLeft -= distance;
+        }, speed);
+        break;
+      case "ArrowDown":
+        auto.style.transform = "rotate(90deg)";
+        autoMovInt = setInterval(() => {
+          auto.style.top = autoTop + distance + "px";
+          autoTop += distance;
+        }, speed);
+        break;
+      case "ArrowUp":
+        auto.style.transform = "rotate(-90deg)";
+        autoMovInt = setInterval(() => {
+          auto.style.top = autoTop - distance + "px";
+          autoTop -= distance;
+        }, speed);
+        break;
+    }
   }
 });
 
 //SAND
 let sand = document.getElementsByClassName("sand");
 let sandArray = Array.from(sand);
-let sandPosition = [];
+let sandPositions = [];
 sandArray.forEach((element) => {
-  sandPosition.push(element.getBoundingClientRect());
+  sandPositions.push(element.getBoundingClientRect());
 });
 
 // KLEBER
-for (let i = 0; i < 30; i++) {
+for (let i = 0; i < 10; i++) {
   let kleber = document.createElement("div");
   document.getElementById("container").appendChild(kleber);
   kleber.setAttribute("class", `kleber kleber-${i}`);
 
-  let kleberPosition = kleber.getBoundingClientRect();
+  let kleberSizes = kleber.getBoundingClientRect();
 
   let rondomNrLeft = Math.floor(
-    Math.random() * (window.innerWidth - kleberPosition.width - 0 + 1)
+    Math.random() * (window.innerWidth - kleberSizes.width - 0 + 1)
   );
   let rondomNrTop = Math.floor(
-    Math.random() * (window.innerHeight - kleberPosition.height - 0 + 1)
+    Math.random() * (window.innerHeight - kleberSizes.height - 0 + 1)
   );
 
   kleber.style.left = `${rondomNrLeft}px`;
   kleber.style.top = `${rondomNrTop}px`;
 
-  kleberPosition = kleber.getBoundingClientRect();
+  let kleberPosition = kleber.getBoundingClientRect();
 
   let kleberLeft = kleberPosition.left;
   let kleberWidth = kleberPosition.width;
@@ -82,54 +88,15 @@ for (let i = 0; i < 30; i++) {
   let kleberHeight = kleberPosition.height;
   let kleberTotalY = kleberTop + kleberHeight;
 
-  if (
-    //SAND1 - HIDE KLEBER
-    (kleberTotalX > sandPosition[0].left &&
-      kleberLeft < sandPosition[0].left + sandPosition[0].width &&
-      kleberTotalY > sandPosition[0].top &&
-      kleberTop < sandPosition[0].top + sandPosition[0].height) ||
-    //SAND2 - HIDE KLEBER
-    (kleberTotalX > sandPosition[1].left &&
-      kleberLeft < sandPosition[1].left + sandPosition[1].width &&
-      kleberTotalY > sandPosition[1].top &&
-      kleberTop < sandPosition[1].top + sandPosition[1].height) ||
-    //SAND3 - HIDE KLEBER
-    (kleberTotalX > sandPosition[2].left &&
-      kleberLeft < sandPosition[2].left + sandPosition[2].width &&
-      kleberTotalY > sandPosition[2].top &&
-      kleberTop < sandPosition[2].top + sandPosition[2].height) ||
-    //SAND4 - HIDE KLEBER
-    (kleberTotalX > sandPosition[3].left &&
-      kleberLeft < sandPosition[3].left + sandPosition[3].width &&
-      kleberTotalY > sandPosition[3].top &&
-      kleberTop < sandPosition[3].top + sandPosition[3].height) ||
-    //SAND5 - HIDE KLEBER
-    (kleberTotalX > sandPosition[4].left &&
-      kleberLeft < sandPosition[4].left + sandPosition[4].width &&
-      kleberTotalY > sandPosition[4].top &&
-      kleberTop < sandPosition[4].top + sandPosition[4].height) ||
-    //SAND6 - HIDE KLEBER
-    (kleberTotalX > sandPosition[5].left &&
-      kleberLeft < sandPosition[5].left + sandPosition[5].width &&
-      kleberTotalY > sandPosition[5].top &&
-      kleberTop < sandPosition[5].top + sandPosition[5].height) ||
-    //SAND7 - HIDE KLEBER
-    (kleberTotalX > sandPosition[6].left &&
-      kleberLeft < sandPosition[6].left + sandPosition[6].width &&
-      kleberTop > sandPosition[6].top &&
-      kleberTop < sandPosition[6].top + sandPosition[6].height) ||
-    //SAND8 - HIDE KLEBER
-    (kleberTotalX > sandPosition[7].left &&
-      kleberLeft < sandPosition[7].left + sandPosition[7].width &&
-      kleberTotalY > sandPosition[7].top &&
-      kleberTop < sandPosition[7].top + sandPosition[7].height) ||
-    //SAND9 - HIDE KLEBER
-    (kleberTotalX > sandPosition[8].left &&
-      kleberLeft < sandPosition[8].left + sandPosition[8].width &&
-      kleberTotalY > sandPosition[8].top &&
-      kleberTop < sandPosition[8].top + sandPosition[8].height)
-  ) {
-    kleber.style.display = "none";
+  for (let e = 0; e < sandPositions.length; e++) {
+    if (
+      kleberTotalX > sandPositions[e].left &&
+      kleberLeft < sandPositions[e].left + sandPositions[e].width &&
+      kleberTotalY > sandPositions[e].top &&
+      kleberTop < sandPositions[e].top + sandPositions[e].height
+    ) {
+      kleber.style.display = "none";
+    }
   }
 }
 
@@ -188,8 +155,10 @@ function check() {
   let autoPosition = auto.getBoundingClientRect();
   let autoLeft = autoPosition.left;
   let autoTop = autoPosition.top;
-  let autoTotalX = autoLeft + autoPosition.width;
-  let autoTotalY = autoTop + autoPosition.height;
+  let autoWidth = autoPosition.width;
+  let autoHeight = autoPosition.height;
+  let autoTotalX = autoLeft + autoWidth;
+  let autoTotalY = autoTop + autoHeight;
   // SCREEN LIMITS
   if (
     autoTop < 0 ||
@@ -199,55 +168,25 @@ function check() {
   ) {
     document.getElementById("crash").style.display = "flex";
     clearInterval(autoMovInt);
+    return;
   }
+
   // ENTERING ON SAND
-  // else if (
-  //   //SAND1 - SLOW DOWN
-  //   (autoTotalX > sandPosition[0].left &&
-  //     autoLeft < sandPosition[0].left + sandPosition[0].width &&
-  //     autoTotalY > sandPosition[0].top &&
-  //     autoTop < sandPosition[0].top + sandPosition[0].height) ||
-  //   //SAND2 - SLOW DOWN
-  //   (kleberTotalX > sandPosition[1].left &&
-  //     kleberLeft < sandPosition[1].left + sandPosition[1].width &&
-  //     kleberTotalY > sandPosition[1].top &&
-  //     kleberTop < sandPosition[1].top + sandPosition[1].height) ||
-  //   //SAND3 - SLOW DOWN
-  //   (kleberTotalX > sandPosition[2].left &&
-  //     kleberLeft < sandPosition[2].left + sandPosition[2].width &&
-  //     kleberTotalY > sandPosition[2].top &&
-  //     kleberTop < sandPosition[2].top + sandPosition[2].height) ||
-  //   //SAND4 - SLOW DOWN
-  //   (kleberTotalX > sandPosition[3].left &&
-  //     kleberLeft < sandPosition[3].left + sandPosition[3].width &&
-  //     kleberTotalY > sandPosition[3].top &&
-  //     kleberTop < sandPosition[3].top + sandPosition[3].height) ||
-  //   //SAND5 - SLOW DOWN
-  //   (kleberTotalX > sandPosition[4].left &&
-  //     kleberLeft < sandPosition[4].left + sandPosition[4].width &&
-  //     kleberTotalY > sandPosition[4].top &&
-  //     kleberTop < sandPosition[4].top + sandPosition[4].height) ||
-  //   //SAND6 - SLOW DOWN
-  //   (kleberTotalX > sandPosition[5].left &&
-  //     kleberLeft < sandPosition[5].left + sandPosition[5].width &&
-  //     kleberTotalY > sandPosition[5].top &&
-  //     kleberTop < sandPosition[5].top + sandPosition[5].height) ||
-  //   //SAND7 - SLOW DOWN
-  //   (kleberTotalX > sandPosition[6].left &&
-  //     kleberLeft < sandPosition[6].left + sandPosition[6].width &&
-  //     kleberTop > sandPosition[6].top &&
-  //     kleberTop < sandPosition[6].top + sandPosition[6].height) ||
-  //   //SAND8 - SLOW DOWN
-  //   (kleberTotalX > sandPosition[7].left &&
-  //     kleberLeft < sandPosition[7].left + sandPosition[7].width &&
-  //     kleberTotalY > sandPosition[7].top &&
-  //     kleberTop < sandPosition[7].top + sandPosition[7].height) ||
-  //   //SAND9 - SLOW DOWN
-  //   (kleberTotalX > sandPosition[8].left &&
-  //     kleberLeft < sandPosition[8].left + sandPosition[8].width &&
-  //     kleberTotalY > sandPosition[8].top &&
-  //     kleberTop < sandPosition[8].top + sandPosition[8].height)
-  // ) {
-  //   speed = 60;
-  // }
+  for (let i = 0; i < sandPositions.length; i++) {
+    if (
+      autoTotalX > sandPositions[i].left &&
+      autoLeft < sandPositions[i].left + sandPositions[i].width &&
+      autoTotalY > sandPositions[i].top &&
+      autoTop < sandPositions[i].top + sandPositions[i].height
+    ) {
+      document.getElementById("crash").style.display = "flex";
+      document.getElementById("sign").style.display = "none";
+      document.getElementById("warning").style.display = "block";
+
+      clearInterval(autoMovInt);
+      clearInterval(checkInterval);
+      isKeyFree = false;
+      break;
+    }
+  }
 }
